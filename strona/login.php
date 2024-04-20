67e60eea8ec5a864711ee08205a66c4afffb4d7e
NEDRI <jedrek.iwaszkiewicz@gmail.com>
2024-04-20 18:46:28 +0200

    potem poprawic

--- strona/login.php
+++ strona/login.php
@@ -1,15 +1,43 @@
 <?php
+session_start();
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
     if ($username === 'admin' && $password === 'admin123') {
-        session_start();
         $_SESSION['username'] = $username;
         header("Location: main/welcome.php");
         exit();
     } else {
-        echo "Wrong username or password";
+        $error = "Wrong username or password";
     }
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logowanie</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="login-container">
+        <h2>Welcome</h2>
+        <form action="login.php" method="post">
+            <div class="input-group">
+                <label for="username">Username:</label>
+                <input type="text" id="username" name="username" required>
+            </div>
+            <div class="input-group">
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+            <?php if(isset($error)) { ?>
+                <p class="error"><?php echo $error; ?></p>
+            <?php } ?>
+            <button type="submit">login</button>
+        </form>
+    </div>
+</body>
+</html>
