1486f20095f45260cad7cd98f7fe991266eb567f
NEDRI <jedrek.iwaszkiewicz@gmail.com>
2024-04-29 11:41:54 +0200

    kaczka

--- strona/main/welcome.php
+++ strona/main/welcome.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Witaj - Strona Główna</title>
+    <title>Welcome</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
@@ -19,22 +19,20 @@
                     <input class="add-btn" type="submit" value="add">
                 </div>
             </div>
-            <h2>Witaj, <?php session_start(); echo $_SESSION['username']; ?>!</h2>
+            <h2>Welcome, <?php session_start(); echo $_SESSION['username']; ?>!</h2>
         </div>    
-        <h3>Ogłoszenia</h3>
-        <div class="ads-table">
-            <div class="grid-container">
-                <div class="grid-item">1</div>
-                <div class="grid-item">2</div>
-                <div class="grid-item">3</div>
-                <div class="grid-item">4</div>
-                <div class="grid-item">5</div>
-                <div class="grid-item">6</div>
-                <div class="grid-item">7</div>
-                <div class="grid-item">8</div>
-                <div class="grid-item">9</div>
-                <div class="grid-item">10</div>
-            </div>
+        <h3>Advertisements:</h3>
+        <div class="grid-container">
+            <div class="grid-item">1</div>
+            <div class="grid-item">2</div>
+            <div class="grid-item">3</div>
+            <div class="grid-item">4</div>
+            <div class="grid-item">5</div>
+            <div class="grid-item">6</div>
+            <div class="grid-item">7</div>
+            <div class="grid-item">8</div>
+            <div class="grid-item">9</div>
+            <div class="grid-item">10</div>
         </div>
     </div>
 </body>
