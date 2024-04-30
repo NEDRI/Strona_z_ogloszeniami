7c7d157ef1bacd23324aa17f7f604c325d1d37a5
NEDRI <jedrek.iwaszkiewicz@gmail.com>
2024-04-30 14:06:05 +0200

    jak jest dodane zdjecie to strona  psuje

--- strona/add/add.php
+++ strona/add/add.php
@@ -48,16 +48,5 @@
             </form>
         </div>
     </div>
-    <script>
-        function previewImage(event) {
-            var reader = new FileReader();
-            reader.onload = function(){
-                var img = document.getElementById('img-preview');
-                img.style.display = 'block';
-                img.src = reader.result;
-            };
-            reader.readAsDataURL(event.target.files[0]);
-        }
-    </script>
 </body>
 </html>
