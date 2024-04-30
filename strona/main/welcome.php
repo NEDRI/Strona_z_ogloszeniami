95539ba6740aaf2ade8232854494c792c22c6d53
NEDRI <jedrek.iwaszkiewicz@gmail.com>
2024-04-30 11:59:03 +0200

    tak

--- strona/main/welcome.php
+++ strona/main/welcome.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="pl">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,7 +16,7 @@
                     </form>
                 </div>
                 <div class="add-container">
-                    <input class="add-btn" type="submit" value="add">
+                    <input class="add-btn" type="button" value="add">
                 </div>
             </div>
             <h2>Welcome, <?php session_start(); echo $_SESSION['username']; ?>!</h2>
