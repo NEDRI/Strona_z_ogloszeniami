fcae3266ad35f01c9a0ec629d6337c1cf7d3e8fc
NEDRI <jedrek.iwaszkiewicz@gmail.com>
2024-05-17 10:10:28 +0200

    logo

--- strona/account/account.php
+++ strona/account/account.php
@@ -1,4 +1,12 @@
-<?php 
-echo "<h1>Jeszcze sie robi</h1>";
-echo '<button class="back-btn" onclick="window.history.back();">Back</button>';
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Account manager</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    
+</body>
+</html>
